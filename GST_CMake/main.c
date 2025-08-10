265786a607ea436c7cee15a3507e66d82153e54e
Santiago Cruz <byte.alpha.tech@gmail.com>
2025-08-09 21:49:51 -0400

    Dynami--Pipelines

--- GST_CMake/main.c
+++ GST_CMake/main.c
@@ -1,48 +1,36 @@
 #include <gst/gst.h>
 
-int main(int argc, char *argv[]) {
-    
+typedef struct _CustomData {
     GstElement *pipeline;
+    GstElement *source;
+    GstElement *convert;
+    GstElement *resample;
+    GstElement *sink;
+} CustomData;
+
+static void pad_added_handler(GstElement *src, GstPad *pad, CustomData *data);
+
+int main(int argc, char *argv[]) {
+    CustomData data;
     GstBus *bus;
     GstMessage *msg;
+    GstStateChangeReturn ret;
+    gboolean terminate = FALSE;
 
     // Initialize GStreamer
     gst_init(&argc, &argv);
 
-    // build the pipeline
-    pipeline = gst_parse_launch(
-        "playbin uri=https://gstreamer.freedesktop.org/data/media/sintel_trailer-480p.webm",
-        NULL);
+    // Create the elements
+    data.source = gst_element_factory_make("uridecodebin", "source");
+    data.convert = gst_element_factory_make("audioconvert", "convert");
+    data.resample = gst_element_factory_make("audioresample", "resample");
+    data.sink = gst_element_factory_make("autoaudiosink", "sink");
 
-    // Start playing
-    gst_element_set_state(pipeline, GST_STATE_PLAYING);
+    // Create the empty pipeline
+    data.pipeline = gst_pipeline_new("test-pipeline");
 
-    // Wait until error or EOS
-    bus = gst_element_get_bus(pipeline);
-    msg = gst_bus_timed_pop_filtered(
-            bus, 
-            GST_CLOCK_TIME_NONE, 
-            GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
-    
-    // Error handling
-    if (GST_MESSAGE_TYPE(msg) == GST_MESSAGE_ERROR) {
-        GError *err;
-        gchar *debug_info;
-
-        gst_message_parse_error(msg, &err, &debug_info);
-        g_printerr("Error received from element %s: %s\n", GST_OBJECT_NAME(msg->src), err->message);
-        g_printerr("Debugging information: %s\n", debug_info ? debug_info : "none");
-        g_clear_error(&err);
-        g_free(debug_info);
-    } else if (GST_MESSAGE_TYPE(msg) == GST_MESSAGE_EOS) {
-        g_print("End-Of-Stream reached.\n");
+    if (!data.pipeline || !data.source || !data.convert || !data.resample || !data.sink) {
+        g_printerr("Not all elements could be created.\n");
+        return -1;
     }
-
-    // Free resources
-    gst_message_unref(msg);
-    gst_object_unref(bus);
-    gst_element_set_state(pipeline, GST_STATE_NULL);
-    gst_object_unref(pipeline);
-
-    return 0;
 }
